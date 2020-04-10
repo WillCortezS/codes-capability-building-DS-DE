ec3926d991e2f62d6bddff1660fc1b4b8e3eb2f5
torroledo <ivan.torroledo@quantumblack.com>
2020-04-10 14:57:16 -0500

    init

---
+++ 4-codigos/2.3.1-script-configuracion.sh
@@ -0,0 +1,14 @@
+#! /bin/bash 
+
+# Crear ambiente virtual 
+conda create --name 'curso_alicorp' python=3.7
+
+# Activar el ambiente virtual 
+conda activate curso_alicorp 
+
+# Instalacion de librerias 
+
+pip install tqdm
+pip install pandas 
+pip install sklearn     
+
